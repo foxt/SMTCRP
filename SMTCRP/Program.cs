bce524d25cc575d7612690907596d6898f903db1
noblereign <nobleds100@gmail.com>
2020-11-24 18:09:28 -0800

    Add project files.

--- SMTCRP/Program.cs
+++ SMTCRP/Program.cs
@@ -47,9 +47,10 @@ namespace SMTCRP
             ShowWindow(handle, SW_HIDE);
         }
         #endregion
-        public static string[] icons = new string[] { "dopamine","winamp","zunemusic" };
+        public static string[] icons = new string[] { "dopamine", "winamp", "zunemusic" };
         public static DiscordRpcClient client;
         public static NotifyIcon trayIcon = new NotifyIcon();
+        public static ContextMenuStrip context = new ContextMenuStrip();
         public static GlobalSystemMediaTransportControlsSessionManager gsmtcsm = GlobalSystemMediaTransportControlsSessionManager.RequestAsync().GetAwaiter().GetResult();
         public static bool isDisplaying = false;
         public static string lastData = "";
@@ -57,16 +58,31 @@ namespace SMTCRP
         public static System.Timers.Timer deferTimer = new System.Timers.Timer(14000);
         public static Icon tray = new Icon("Resources/tray.ico");
         public static Icon trey = new Icon("Resources/trey.ico");
+        public static Icon troy = new Icon("Resources/troy.ico");
+
+        public static ToolStripMenuItem showalbumtitle = new ToolStripMenuItem("Show album title");
+        public static ToolStripMenuItem showtracknumber = new ToolStripMenuItem("Show track number");
+        public static ToolStripMenuItem trayonstart = new ToolStripMenuItem("Tray on startup");
+        public static ToolStripMenuItem blockbrowsers = new ToolStripMenuItem("Auto block browsers");
+        public static ToolStripMenuItem blockapp = new ToolStripMenuItem("Block current app");
+
+        public static string currentSession = null;
+
         static void Main(string[] args)
         {
             #region UI
             AllocConsole();
-            Console.Title = "SMTCRP Log - Close this window to quit SMTCRP";
-            HideConsoleWindow();
-            bool consoleShowing = false;
-            trayIcon.Text = "SMTCRP";
-            trayIcon.Icon = tray;
-            trayIcon.DoubleClick += (object sender, EventArgs evt) =>
+            Console.Title = "SMTCRP Log - Closing this window quits SMTCRP";
+            bool consoleShowing = !Properties.Settings.Default.trayOnStartup;
+            trayIcon.Text = "SMTCRP - Waiting";
+            trayIcon.Icon = trey;
+
+            if (!consoleShowing)
+            {
+                HideConsoleWindow();
+            }
+
+            context.Items.Add("Toggle Console", null, (object sender, EventArgs evt) =>
             {
                 Console.WriteLine("User clicked tray icon");
                 if (consoleShowing)
@@ -79,7 +95,71 @@ namespace SMTCRP
                     ShowConsoleWindow();
                     consoleShowing = true;
                 }
+            });
+
+            showalbumtitle.Checked = Properties.Settings.Default.useAlbumTitle;
+            showalbumtitle.Click += (object sender, EventArgs evt) =>
+            {
+                Properties.Settings.Default.useAlbumTitle = !Properties.Settings.Default.useAlbumTitle;
+                Properties.Settings.Default.Save();
+                showalbumtitle.Checked = Properties.Settings.Default.useAlbumTitle;
             };
+            showtracknumber.Checked = Properties.Settings.Default.useTrackNumbers;
+            showtracknumber.Click += (object sender, EventArgs evt) =>
+            {
+                Properties.Settings.Default.useTrackNumbers = !Properties.Settings.Default.useTrackNumbers;
+                Properties.Settings.Default.Save();
+                showtracknumber.Checked = Properties.Settings.Default.useTrackNumbers;
+            };
+            trayonstart.Checked = Properties.Settings.Default.trayOnStartup;
+            trayonstart.Click += (object sender, EventArgs evt) =>
+            {
+                Properties.Settings.Default.trayOnStartup = !Properties.Settings.Default.trayOnStartup;
+                Properties.Settings.Default.Save();
+                trayonstart.Checked = Properties.Settings.Default.trayOnStartup;
+            };
+            blockbrowsers.Checked = Properties.Settings.Default.blockBrowsers;
+            blockbrowsers.Click += (object sender, EventArgs evt) =>
+            {
+                Properties.Settings.Default.blockBrowsers = !Properties.Settings.Default.blockBrowsers;
+                Properties.Settings.Default.Save();
+                blockbrowsers.Checked = Properties.Settings.Default.blockBrowsers;
+            };
+            blockapp.Enabled = false;
+            blockapp.Click += (object sender, EventArgs evt) =>
+            {
+                if (Properties.Settings.Default.blockedApps.Contains(currentSession))
+                {
+                    Properties.Settings.Default.blockedApps.Remove(currentSession);
+                }
+                else
+                {
+                    Properties.Settings.Default.blockedApps.Add(currentSession);
+                }
+                Properties.Settings.Default.Save();
+                blockapp.Checked = Properties.Settings.Default.blockedApps.Contains(currentSession);
+            };
+
+
+            Properties.Settings.Default.SettingsSaving += (object sender, System.ComponentModel.CancelEventArgs evt) =>
+            {
+                Console.WriteLine("Settings updated, Presence will update as soon as it's allowed");
+                isDisplaying = false;
+            };
+
+            context.Items.Add(showalbumtitle);
+            context.Items.Add(showtracknumber);
+            context.Items.Add(blockbrowsers);
+            context.Items.Add(blockapp);
+            context.Items.Add(trayonstart);
+
+            context.Items.Add("Exit SMTCRP", null, (object sender, EventArgs evt) =>
+            {
+                client.Dispose();
+                Application.Exit();
+            });
+
+            trayIcon.ContextMenuStrip = context;
 
             trayIcon.Visible = true;
             #endregion
@@ -91,7 +171,7 @@ namespace SMTCRP
             };
             client.OnReady += (sender, e) =>
             {
-                string[] greetings = new string[] { "Howdy", "Hi","Hey","Welcome", "Bonjour","Hola","Ni hao", "Ciao", "Konnichiwa","Guten Tag","Ola","Hej"};
+                string[] greetings = new string[] { "Howdy", "Hi", "Hey", "Welcome", "Bonjour", "Hola", "Ni hao", "Ciao", "Konnichiwa", "Guten Tag", "Ola", "Hej", "Yo", "What's up", "Kamusta" };
                 Console.WriteLine("{1}, {0}!", e.User.Username, greetings[new Random().Next(0, greetings.Length)]);
                 if (!updTimer.Enabled && !deferTimer.Enabled)
                 {
@@ -101,7 +181,14 @@ namespace SMTCRP
 
             client.OnPresenceUpdate += (sender, e) =>
             {
-                Console.WriteLine("Recieved confirmation that presence is {0}", e.Presence.Details);
+                try
+                {
+                    Console.WriteLine("Recieved confirmation that presence is {0}", e.Presence.Details);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Presence cleared!");
+                }
             };
 
             client.Initialize();
@@ -114,7 +201,7 @@ namespace SMTCRP
             deferTimer.AutoReset = false;
             deferTimer.Enabled = false;
             Application.Run();
-            
+
         }
 
         private static void RestartUpdateTimer(object sender, ElapsedEventArgs e)
@@ -133,22 +220,46 @@ namespace SMTCRP
                 var session = gsmtcsm.GetCurrentSession();
                 var ses = new List<GlobalSystemMediaTransportControlsSession>(gsmtcsm.GetSessions());
                 var i = 0;
-                //TODO: make this nicer
-                while (session.SourceAppUserModelId.ToLower().StartsWith("discord") ||
-                    session.SourceAppUserModelId.ToLower().StartsWith("firefox") ||
-                    session.SourceAppUserModelId.ToLower().StartsWith("chrom") ||
-                    session.SourceAppUserModelId.ToLower().StartsWith("opera") ||
-                    session.SourceAppUserModelId.ToLower().StartsWith("spotify"))
+
+                if (session != null)
                 {
-                    Console.WriteLine("Ignoring {0}", session.SourceAppUserModelId);
-                    if (session == null || i >= ses.Count)
+                    currentSession = session.SourceAppUserModelId.ToLower();
+
+                    if (blockapp.GetCurrentParent().InvokeRequired)
                     {
-                        session = null;
-                        break;
+                        blockapp.GetCurrentParent().Invoke(new Action(() =>
+                        {
+                        // Do anything you want with the control here
+                        blockapp.Checked = Properties.Settings.Default.blockedApps.Contains(currentSession);
+                            blockapp.Enabled = true;
+                        }));
                     }
-                    session = ses[i];
-                    i += 1;
+                    else
+                    {
+                        blockapp.Checked = Properties.Settings.Default.blockedApps.Contains(currentSession);
+                        blockapp.Enabled = true;
+                    }
                 }
+                else
+                {
+                    currentSession = null;
+                    if (blockapp.GetCurrentParent().InvokeRequired)
+                    {
+                        blockapp.GetCurrentParent().Invoke(new Action(() =>
+                        {
+                            blockapp.Text = "No media session";
+                            blockapp.Checked = false;
+                            blockapp.Enabled = false;
+                        }));
+                    }
+                    else
+                    {
+                        blockapp.Text = "No media session";
+                        blockapp.Checked = false;
+                        blockapp.Enabled = false;
+                    }
+                }
+
                 if (session == null)
                 {
                     trayIcon.Icon = trey;
@@ -164,27 +275,32 @@ namespace SMTCRP
                     }
                     return;
                 }
-                string appName = session.SourceAppUserModelId.Replace(".exe", "");
-                var pbi = session.GetPlaybackInfo();
-                if (pbi.PlaybackStatus != GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing && !isDisplaying)
+                else
                 {
-                    return;
+                    //TODO: make this nicer
+                    if (Properties.Settings.Default.blockBrowsers)
+                    {
+                        while (session.SourceAppUserModelId.ToLower().StartsWith("discord") ||
+                            session.SourceAppUserModelId.ToLower().StartsWith("firefox") ||
+                            session.SourceAppUserModelId.ToLower().StartsWith("chrom") ||
+                            session.SourceAppUserModelId.ToLower().StartsWith("opera") ||
+                            session.SourceAppUserModelId.ToLower().StartsWith("msedge") ||
+                            session.SourceAppUserModelId.ToLower().StartsWith("spotify"))
+                        {
+                            Console.WriteLine("browser_blocker {0}", session.SourceAppUserModelId);
+                            if (session == null || i >= ses.Count)
+                            {
+                                session = null;
+                                break;
+                            }
+                            session = ses[i];
+                            i += 1;
+                        }
+                    }
                 }
-                if (pbi.PlaybackStatus != GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing)
-                {
-                    trayIcon.Icon = trey;
-                    trayIcon.Text = "SMTCRP - Nothing playing";
-                    client.ClearPresence();
-                    isDisplaying = false;
-                    Console.WriteLine("Playback is {0}, Defering update cycle for 14 seconds...", pbi.PlaybackStatus);
-                    updTimer.Stop();
-                    deferTimer.Start();
-                    return;
-                }
-                var tlp = session.GetTimelineProperties();
+
                 var mediaProperties = session.TryGetMediaPropertiesAsync().GetAwaiter().GetResult();
-
-
+                string appName = session.SourceAppUserModelId.Replace(".exe", "");
                 string appLogo = "generic";
                 if (icons.Contains(appName.ToLower()))
                 {
@@ -209,29 +325,102 @@ namespace SMTCRP
                     appName = appName.Split(".").Last();
                 }
                 var data = mediaProperties.Title + " by " + mediaProperties.Artist + " via " + appName;
+                if (blockapp.GetCurrentParent().InvokeRequired)
+                {
+                    blockapp.GetCurrentParent().Invoke(new Action(() =>
+                    {
+                        blockapp.Text = "Block " + appName;
+                    }));
+                }
+                else
+                {
+                    blockapp.Text = "Block " + appName;
+                }
+
+                var pbi = session.GetPlaybackInfo();
+                if (pbi.PlaybackStatus != GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing && !isDisplaying)
+                {
+                    return;
+                }
+                if (pbi.PlaybackStatus != GlobalSystemMediaTransportControlsSessionPlaybackStatus.Playing)
+                {
+                    trayIcon.Icon = trey;
+                    trayIcon.Text = "SMTCRP - Nothing playing";
+                    client.ClearPresence();
+                    isDisplaying = false;
+                    Console.WriteLine("Playback is {0}, Defering update cycle for 14 seconds...", pbi.PlaybackStatus);
+                    updTimer.Stop();
+                    deferTimer.Start();
+                    return;
+                }
+                var tlp = session.GetTimelineProperties();
+
                 if (data == lastData && isDisplaying) { return; }
                 Console.WriteLine(data);
-                client.SetPresence(new RichPresence()
+
+                var count = mediaProperties.AlbumTrackCount;
+                var num = mediaProperties.TrackNumber;
+
+                if (count < num)
                 {
-                    Details = "🎵" + mediaProperties.Title,
-                    State = "👤" + mediaProperties.Artist,
-                    Assets = new Assets()
+                    count = num;
+                }
+
+                if (Properties.Settings.Default.blockedApps.Contains(session.SourceAppUserModelId.ToLower()))
+                {
+                    Console.WriteLine("user_blocker {0}", session.SourceAppUserModelId);
+                    trayIcon.Icon = troy;
+                    trayIcon.Text = "SMTCRP - " + appName + " is blocked";
+                    client.ClearPresence();
+                    isDisplaying = false;
+                    Console.WriteLine("Playback is {0}, Defering update cycle for 14 seconds...", pbi.PlaybackStatus);
+                    updTimer.Stop();
+                    deferTimer.Start();
+                }
+                else
+                {
+                    var generated_party = new Party()
                     {
-                        LargeImageKey = appLogo,
-                        LargeImageText = "via " + appName,
-                        SmallImageKey = "logo",
-                        SmallImageText = "https://github.com/thelmgn/smtcrp"
+                        ID = Guid.NewGuid().ToString(),
+                        Size = num,
+                        Max = count
+                    };
+
+                    if (!Properties.Settings.Default.useTrackNumbers)
+                    {
+                        generated_party = null;
                     }
-                });
-                isDisplaying = true;
-                lastData = data;
-                trayIcon.Icon = tray;
-                if (data.Length > 54)
-                {
-                    trayIcon.Text = "SMTCRP - " + data.Substring(0, 53) + "…";
-                } else
-                {
-                    trayIcon.Text = "SMTCRP - " + data;
+
+                    var generated_details = "🎵 " + mediaProperties.Title;
+                    if (Properties.Settings.Default.useAlbumTitle)
+                    {
+                        generated_details = "🎵 " + mediaProperties.AlbumTitle + " - " + mediaProperties.Title;
+                    }
+
+                    client.SetPresence(new RichPresence()
+                    {
+                        Details = generated_details,
+                        State = "👥 " + mediaProperties.Artist,
+                        Party = generated_party,
+                        Assets = new Assets()
+                        {
+                            LargeImageKey = appLogo,
+                            LargeImageText = "via " + appName,
+                            SmallImageKey = "logo",
+                            SmallImageText = "https://github.com/thelmgn/smtcrp"
+                        },
+                    });
+                    isDisplaying = true;
+                    lastData = data;
+                    trayIcon.Icon = tray;
+                    if (data.Length > 54)
+                    {
+                        trayIcon.Text = "SMTCRP - " + data.Substring(0, 53) + "…";
+                    }
+                    else
+                    {
+                        trayIcon.Text = "SMTCRP - " + data;
+                    }
                 }
                 Console.WriteLine("Defering update cycle for 14 seconds...");
                 updTimer.Stop();
